de890f221ae6e390da9f01028e727aa44ffaf268
Yogen Rai <yogen.rai.992@gmail.com>
2018-10-25 21:04:24 -0700

    Added logging with log4j2

--- src/main/java/com/bitMiners/pdf/controllers/LoginController.java
+++ src/main/java/com/bitMiners/pdf/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package com.bitMiners.pdf.controllers;
 
 import com.bitMiners.pdf.services.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpSession;
 @SessionAttributes({"currentUser", "currentUserId"})
 @Controller
 public class LoginController {
+    private static final Logger log = LogManager.getLogger(LoginController.class);
 
     @Autowired
     private UserService userService;
@@ -45,17 +48,20 @@ public class LoginController {
 
     @RequestMapping(value = "/postLogin", method = RequestMethod.POST)
     public String postLogin(String username, String password, Model model, HttpSession session) {
+        log.info("postLogin()-> username : {}", username);
         if (username == null || password == null) {
             model.addAttribute("message", "None of field can be empty");
         }
         boolean isCorrect = userService.isCorrectUsernameAndPassword(username, password);
-        if (isCorrect) {
-            model.addAttribute("currentUserId", userService.getUserByUsername(username).getId());
-            model.addAttribute("currentUser", username);
-            session.setAttribute("userId", userService.getUserByUsername(username).getId());
-            return "redirect:/wallPage";
+        if (!isCorrect) {
+            log.info("Login failure for user: {}", username);
+            model.addAttribute("message", "Username or password didn't matched");
+            return "login";
         }
-        model.addAttribute("message", "Username or password didn't matched");
-        return "login";
+        log.info("Login success for user: {}", username);
+        model.addAttribute("currentUserId", userService.getUserByUsername(username).getId());
+        model.addAttribute("currentUser", username);
+        session.setAttribute("userId", userService.getUserByUsername(username).getId());
+        return "redirect:/wallPage";
     }
 }
