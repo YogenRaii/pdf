92937b08d7ce71d2cc4eef6aa024dcd380b0de42
Yogen Rai <yogen.rai.992@gmail.com>
2019-01-12 13:55:02 -0800

    Updated query to retrieve admins with native sql query (#12)

--- pdf-app/src/main/java/com/bitMiners/pdf/repositories/impl/UserRepositoryImpl.java
+++ pdf-app/src/main/java/com/bitMiners/pdf/repositories/impl/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.bitMiners.pdf.repositories.impl;
 import com.bitMiners.pdf.domain.User;
 import com.bitMiners.pdf.repositories.UserRepository;
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -49,8 +50,9 @@ public class UserRepositoryImpl implements UserRepository {
 
     @SuppressWarnings("unchecked")
     public List<User> getAllAdmins() {
-        Query query = sessionFactory.getCurrentSession().createQuery("from User u where u.role=:role");
+        SQLQuery query = sessionFactory.getCurrentSession().createSQLQuery("SELECT * from User u where id in (select user_id from user_authority where authority_id=(select id from authority where name=:role))");
         query.setParameter("role", "ROLE_ADMIN");
+        query.addEntity(User.class);
         return query.list();
     }
 }
