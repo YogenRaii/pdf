cba3d8fc06773bb00c46088dc0a5ebf9d56bfa47
Yogen Rai <yogen.rai.992@gmail.com>
2019-01-19 20:19:50 -0800

    Refactoring around advice and signup flow

--- pdf-app/src/main/java/com/bitMiners/pdf/advice/SignupAdvice.java
+++ pdf-app/src/main/java/com/bitMiners/pdf/advice/SignupAdvice.java
@@ -4,6 +4,7 @@ import com.bitMiners.pdf.domain.User;
 import com.bitMiners.pdf.email.EmailService;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ public class SignupAdvice {
     @Autowired
     private EmailService emailService;
 
-    @After("execution(* com.bitMiners.pdf.repositories.impl.UserRepositoryImpl.add(..))")
+    @AfterReturning("execution(* com.bitMiners.pdf.services.impl.UserServiceImpl.addUser(..))")
     public void aopEmail(JoinPoint jp) {
         User user = (User) jp.getArgs()[0];
         emailService.sendEmail(user.getEmail(), "Registration info", "Registered Sucessfully to Programming discussion forum!! " +
